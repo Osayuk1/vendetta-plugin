54fe89f05f623170e9134421314ef7d19dce1a40
Osayuk1 <157780677+Osayuk1@users.noreply.github.com>
2025-05-24 12:49:34 +0100

    Create index.ts

---
+++ save-deleted/src/index.ts
@@ -0,0 +1,25 @@
+import { before } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro";
+import { storage } from "@vendetta/plugin";
+
+const MessageStore = findByProps("deleteMessage", "getMessage");
+
+let unpatch;
+
+export default {
+  onLoad: () => {
+    if (!storage.deletedMessages) storage.deletedMessages = {};
+
+    unpatch = before("deleteMessage", MessageStore, ([channelId, messageId]) => {
+      const msg = MessageStore.getMessage(channelId, messageId);
+      if (msg) {
+        if (!storage.deletedMessages[channelId])
+          storage.deletedMessages[channelId] = {};
+        storage.deletedMessages[channelId][messageId] = msg;
+      }
+    });
+  },
+  onUnload: () => {
+    unpatch?.();
+  }
+};
